a195419d6e32bc4af6f2c4bb0d208ef23ca2492c
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2023-11-25 15:56:22 +0000

    Fix WebGL program info log retrieval

--- js/webassembly.js
+++ js/webassembly.js
@@ -56,34 +56,32 @@ function idToObject(id) {
 function gl_getContext(canvas) {
     return objectToId(document.getElementById(getString(canvas)).getContext("webgl"));
 }
-
 function gl_createProgram(gl) {
     return objectToId(idToObject(gl).createProgram());
 }
-
 function gl_attachShader(gl, program, shader) {
     return idToObject(gl).attachShader(idToObject(program), getString(shader));
 }
-
 function gl_linkProgram(gl, program) {
     return idToObject(gl).linkProgram(idToObject(program));
 }
-
 function gl_getProgramParameter(gl, program, pname) {
     return idToObject(gl).getProgramParameter(idToObject(program), pname);
 }
+
+function gl_getProgramInfoLog(gl, program) {
+    return stringToPtr(idToObject(gl).getProgramInfoLog(idToObject(program)));
+}
+
 function gl_createShader(gl, type) {
     return objectToId(idToObject(gl).createShader(type));
 }
-
 function gl_shaderSource(gl, shader, source) {
     idToObject(gl).shaderSource(idToObject(shader), getString(source));
 }
-
 function gl_compileShader(gl, shader) {
     idToObject(gl).compileShader(idToObject(shader));
 }
-
 function gl_getShaderParameter(gl, shader, pname) {
     return idToObject(gl).getShaderParameter(idToObject(shader), pname);
 }
