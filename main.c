4a0f1a7386674bc8a6ceb95040ed4b7d82cd392e
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2023-11-25 10:47:32 +0000

    Fix string formatting bug and uncomment debug print statement

--- main.c
+++ main.c
@@ -2,9 +2,10 @@
 
 int main()
 {
-    const char *asd = "asd";
+    const char *hello = "Hello World!";
     int drei = 3;
 
-    printf("The answer is %d and %d\n", drei, 20 + 4);
+    // printf("The answer is %d and %d\n", drei, 20 + 4);
+    printf("The answer is %s\n", hello);
     return drei + 5;
 }
