a97791f2bf5aaca93e5d5ed1124a2c74502163d1
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2023-11-25 10:40:12 +0000

    Refactor code to remove unused variables and update printf statements

--- main.c
+++ main.c
@@ -1,30 +1,10 @@
-#include <stdarg.h>
-#include <stdint.h>
-
-extern int printf_js(const char *format, void *args);
-int printf(const char *format, ...)
-{
-    va_list args_va;
-    va_start(args_va, format);
-    void *args_arr[3];
-
-    for (unsigned int i = 0; i < 3; i++)
-    {
-        args_arr[i] = va_arg(args_va, void *);
-    }
-
-    va_end(args_va);
-
-    printf_js(format, args_arr);
-    return 0;
-}
+#include "webassembly.h"
 
 int main()
 {
     const char *asd = "asd";
     int drei = 3;
 
-    printf(asd);
-    printf("The answer is %f and %i and %d \n", 1222.3, drei, 20 + 4);
-    return 0;
+    printf("The answer is %d and %d\n", drei, 20 + 4);
+    return drei + 5;
 }
--- webassembly.h
+++ webassembly.h
@@ -1,14 +1,13 @@
 #include <stdarg.h>
-#include <stdint.h>
 
 extern int printf_js(const char *format, void *args);
 int printf(const char *format, ...)
 {
     va_list args_va;
     va_start(args_va, format);
-    void *args_arr[3];
+    void *args_arr[2];
 
-    for (unsigned int i = 0; i < 3; i++)
+    for (unsigned int i = 0; i < 2; i++)
     {
         args_arr[i] = va_arg(args_va, void *);
     }
