6833883893e3877f69a4579bfcb58726e4481e38
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2023-11-25 13:48:58 +0000

    Add WebGL shader functions and buffer management

--- js/webassembly.js
+++ js/webassembly.js
@@ -10,12 +10,19 @@ function getString(ptr) {
     return str;
 }
 
+function stringToPtr(str) {
+    let bytearray = new Uint8Array(memorybuffer);
+    for (let i = 0; i < str.length; i++) {
+        bytearray[i] = str.charCodeAt(i);
+    }
+    bytearray[str.length] = 0;
+    return bytearray.byteOffset;
+}
+
 // Printing
 function print(format, argptr) {
     str = getString(format);
-
     let argIndex = 0;
-
     console.log(str.replace(/%[sd]/g, (match) => {
         switch (match) {
             case '%s': {
@@ -32,7 +39,6 @@ function print(format, argptr) {
         }
     }));
 }
-
 
 // WebGL
 let id = 0;
@@ -61,4 +67,71 @@ function gl_attachShader(gl, program, shader) {
 
 function gl_linkProgram(gl, program) {
     return idToObject(gl).linkProgram(idToObject(program));
+}
+
+function gl_getProgramParameter(gl, program, pname) {
+    return idToObject(gl).getProgramParameter(idToObject(program), pname);
+}
+function gl_createShader(gl, type) {
+    return objectToId(idToObject(gl).createShader(type));
+}
+
+function gl_shaderSource(gl, shader, source) {
+    idToObject(gl).shaderSource(idToObject(shader), getString(source));
+}
+
+function gl_compileShader(gl, shader) {
+    idToObject(gl).compileShader(idToObject(shader));
+}
+
+function gl_getShaderParameter(gl, shader, pname) {
+    return idToObject(gl).getShaderParameter(idToObject(shader), pname);
+}
+
+function gl_getShaderInfoLog(gl, shader) {
+    return stringToPtr(idToObject(gl).getShaderInfoLog(idToObject(shader))); // Assumes stringToPtr is a function that converts JS string to Wasm memory pointer
+}
+
+// Buffer Management
+function gl_createBuffer(gl) {
+    return objectToId(idToObject(gl).createBuffer());
+}
+
+function gl_bindBuffer(gl, target, buffer) {
+    idToObject(gl).bindBuffer(target, idToObject(buffer));
+}
+
+function gl_bufferData(gl, target, size, data, usage) {
+    var dataArray = new Float32Array(memorybuffer, data, size / Float32Array.BYTES_PER_ELEMENT);
+    idToObject(gl).bufferData(target, dataArray, usage);
+}
+
+// Uniform and Attribute Management
+function gl_getUniformLocation(gl, program, name) {
+    return objectToId(idToObject(gl).getUniformLocation(idToObject(program), getString(name)));
+}
+
+function gl_uniformMatrix4fv(gl, location, transpose, value) {
+    var valueArray = new Float32Array(memorybuffer, value, 16); // 16 for 4x4 matrix
+    idToObject(gl).uniformMatrix4fv(idToObject(location), transpose, valueArray);
+}
+
+function gl_getAttribLocation(gl, program, name) {
+    return idToObject(gl).getAttribLocation(idToObject(program), getString(name));
+}
+
+function gl_vertexAttribPointer(gl, index, size, type, normalized, stride, offset) {
+    idToObject(gl).vertexAttribPointer(index, size, type, normalized, stride, offset);
+}
+
+function gl_enableVertexAttribArray(gl, index) {
+    idToObject(gl).enableVertexAttribArray(index);
+}
+
+function gl_createTexture(gl) {
+    return objectToId(idToObject(gl).createTexture());
+}
+
+function gl_bindTexture(gl, target, texture) {
+    idToObject(gl).bindTexture(target, idToObject(texture));
 }
--- src/webassembly.h
+++ src/webassembly.h
@@ -38,7 +38,38 @@ int printf(const char *format, ...)
 }
 
 // WebGL
+typedef enum
+{
+    VERTEX_SHADER = 0x8B31,
+    FRAGMENT_SHADER = 0x8B30,
+    COMPILE_STATUS = 0x8B81,
+    ARRAY_BUFFER = 0x8892,
+    STATIC_DRAW = 0x88E4,
+    FLOAT = 0x1406,
+    LINK_STATUS = 0x8B82,
+} GLenum;
+
 extern unsigned int gl_getContext(const char *canvas);
 extern unsigned int gl_createProgram(unsigned int gl);
 extern unsigned int gl_attachShader(unsigned int gl, unsigned int program, const char *shader);
 extern unsigned int gl_linkProgram(unsigned int gl, unsigned int program);
+extern unsigned int gl_createShader(unsigned int gl, GLenum type);
+extern void gl_shaderSource(unsigned int gl, unsigned int shader, const char *source);
+extern void gl_compileShader(unsigned int gl, unsigned int shader);
+extern int gl_getShaderParameter(unsigned int gl, unsigned int shader, GLenum pname);
+extern void gl_getShaderInfoLog(unsigned int gl, unsigned int shader, char *infoLog);
+extern unsigned int gl_createBuffer(unsigned int gl);
+extern void gl_bindBuffer(unsigned int gl, GLenum target, unsigned int buffer);
+extern void gl_bufferData(unsigned int gl, GLenum target, unsigned int size, const void *data, GLenum usage);
+
+// Uniforms and Attributes
+typedef struct
+{
+    unsigned int location;
+} GLUniformLocation;
+
+extern GLUniformLocation gl_getUniformLocation(unsigned int gl, unsigned int program, const char *name);
+extern void gl_uniformMatrix4fv(unsigned int gl, GLUniformLocation location, int count, int transpose, const float *value);
+extern int gl_getAttribLocation(unsigned int gl, unsigned int program, const char *name);
+extern void gl_vertexAttribPointer(unsigned int gl, int index, int size, GLenum type, int normalized, int stride, const void *pointer);
+extern void gl_enableVertexAttribArray(unsigned int gl, int index);
