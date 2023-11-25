52c7b42f626981d568b6c0e7f7f7d07362993236
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2023-11-25 11:10:53 +0000

    Update printf function to support variable number of arguments

--- main.c
+++ main.c
@@ -5,6 +5,6 @@ int main()
     const char *hello = "Hello World!";
     int drei = 3;
 
-    printf("The answer is %d and %s\n", drei, hello);
+    printf("The answer is %d and %s and %d\n", drei, hello, 244 + 2222);
     return drei + 5;
 }
--- webassembly.h
+++ webassembly.h
@@ -1,13 +1,34 @@
 #include <stdarg.h>
 
 extern int printf_js(const char *format, void *args);
+int count_format_specifiers(const char *format)
+{
+    int count = 0;
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
+            if (*(format + 1) != '\0')
+            {
+                if (*(format + 1) == 'd' || *(format + 1) == 's')
+                {
+                    count++;
+                    format++;
+                }
+            }
+        }
+        format++;
+    }
+    return count;
+}
 int printf(const char *format, ...)
 {
+    unsigned int count = count_format_specifiers(format);
     va_list args_va;
     va_start(args_va, format);
-    void *args_arr[2];
+    void *args_arr[count];
 
-    for (unsigned int i = 0; i < 2; i++)
+    for (unsigned int i = 0; i < count; i++)
     {
         args_arr[i] = va_arg(args_va, void *);
     }
