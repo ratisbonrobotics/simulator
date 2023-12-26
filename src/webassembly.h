2a983b66f24b1215afc8df57e1a10fd086f0f032
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2023-12-26 08:20:03 +0000

    Remove unused code and settings

--- src/webassembly.h
+++
@@ -1,7 +0,0 @@
-int printf(const char *format, ...);
-void ftoa(float n, char *res, int afterpoint);
-extern double fmod(double x, double y);
-extern float sinf(float x);
-extern float cosf(float x);
-extern float tanf(float x);
-extern float sqrtf(float x);
