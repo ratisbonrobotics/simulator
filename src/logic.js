aef54b9cdfdc4cae36a47eb0f2aa9ff19f0a10b3
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2023-12-27 20:06:31 +0000

    Fix rotation bug in createXRotationMatrix and adjust droneLogic timeout

--- src/logic.js
+++ src/logic.js
@@ -1,11 +1,12 @@
 // ----------------------------------- LOGIC -----------------------------------
-droneModelMatrix = createModelMatrix(0.0, 0.0, 0.0, 45.0, 0.0, 0.0, 1.0, 1.0, 1.0);
+droneModelMatrix = createModelMatrix(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0);
 
 droneLogic();
 function droneLogic() {
 	if (running) {
+		droneModelMatrix = mult(createZRotationMatrix(degreeToRadians(0.1)), droneModelMatrix);
 		// here we need to adjust the drones modelmatrix according to the speed of the rotors.
 
 	}
-	setTimeout(droneLogic, 100);
+	setTimeout(droneLogic, 10);
 }
