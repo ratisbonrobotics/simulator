4cd4c1199e74b4d3f811542cd4fca92ab7936551
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2023-12-27 20:10:40 +0000

    Update navbar link and adjust drone rotation

--- src/logic.js
+++ src/logic.js
@@ -4,7 +4,7 @@ droneModelMatrix = createModelMatrix(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0
 droneLogic();
 function droneLogic() {
 	if (running) {
-		droneModelMatrix = mult(createZRotationMatrix(degreeToRadians(0.1)), droneModelMatrix);
+		droneModelMatrix = mult(createYRotationMatrix(degreeToRadians(0.1)), droneModelMatrix);
 		// here we need to adjust the drones modelmatrix according to the speed of the rotors.
 
 	}
