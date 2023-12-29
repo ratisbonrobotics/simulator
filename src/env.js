cca5750bd1eb5e126a13ede60717ac2b84d3a381
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2023-12-29 10:10:57 +0000

    Update canvas variable and add graphicx package

--- src/env.js
+++ src/env.js
@@ -3,17 +3,17 @@ var running = true;
 var keys = {};
 var viewxz = 0;
 var viewy = 0;
-var fscanvas = document.getElementById("canvas");
-var gl = fscanvas.getContext("webgl");
+var canvas = document.getElementById("canvas");
+var gl = canvas.getContext("webgl");
 resizeCanvas();
 
 // --- ADD EVENT LISTENERS ---
 window.addEventListener("orientationchange", resizeCanvas);
 window.addEventListener("resize", resizeCanvas);
 document.getElementById("startstop").addEventListener("click", toggle);
-fscanvas.addEventListener("mousemove", function (event) {
-    if (document.pointerLockElement === fscanvas && running) { updatePosition(event); }
+canvas.addEventListener("mousemove", function (event) {
+    if (document.pointerLockElement === canvas && running) { updatePosition(event); }
 });
-fscanvas.addEventListener("click", fscanvas.requestPointerLock);
-fscanvas.addEventListener("keydown", function (event) { keys[event.key] = true; });
-fscanvas.addEventListener("keyup", function (event) { keys[event.key] = false; });
+canvas.addEventListener("click", canvas.requestPointerLock);
+canvas.addEventListener("keydown", function (event) { keys[event.key] = true; });
+canvas.addEventListener("keyup", function (event) { keys[event.key] = false; });
