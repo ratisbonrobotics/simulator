23c90354c934a3dd45c687e598975380b121e543
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-01-04 08:29:02 +0000

    Add drone dynamics and temporary helper variables

--- src/dynamics.js
+++ src/dynamics.js
@@ -1,5 +1,18 @@
-// ----------------------------------- ENV + UTIL -----------------------------------
-
+// ----------------------------------- TEMP HELPER -----------------------------------
+const k_f = 0.00141446535;
+const k_m = 0.0004215641;
+const m = 1.0;
+const L = 0.23;
+const i_x = 0.0121;
+const i_y = 0.0119;
+const i_z = 0.0223;
+const g = 9.81;
+const omega_min = 20
+const omega_max = 66
+var omega_1 = 45.0;
+var omega_2 = 45.0;
+var omega_3 = 45.0;
+var omega_4 = 45.0;
 var paused = false;
 
 setInterval(() => {
@@ -9,6 +22,20 @@ setInterval(() => {
 	}
 }, 5000);
 
+setInterval(() => {
+	if (!paused) {
+		console.log("x: " + d[0].toFixed(1) +
+			" y: " + d[1].toFixed(1) +
+			" z: " + d[2].toFixed(1) +
+			" theta: " + d[3].toFixed(1) +
+			" phi: " + d[4].toFixed(1) +
+			" psi: " + d[5].toFixed(1));
+		console.log("omega_1: " + omega_1.toFixed(1) +
+			" omega_2: " + omega_2.toFixed(1) +
+			" omega_3: " + omega_3.toFixed(1) +
+			" omega_4: " + omega_4.toFixed(1));
+	}
+}, 1000);
 
 setInterval(() => {
 	if (keys["p"]) {
@@ -37,66 +64,30 @@ setInterval(() => {
 
 	}
 
+	droneModelMatrix = createModelMatrix(d[0], d[1], d[2], d[3], d[4], d[5], 1.0, 1.0, 1.0);
+
 }, 10);
 
-const k_f = 0.00141446535;
-const k_m = 0.0004215641;
-const m = 1.0;
-const L = 0.23;
-const i_x = 0.0121;
-const i_y = 0.0119;
-const i_z = 0.0223;
-const g = 9.81;
-const omega_min = 20
-const omega_max = 66
 
-// ----------------------------------- LOGIC -----------------------------------
-droneModelMatrix = createModelMatrix(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0);
-terrainModelMatrix = createModelMatrix(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0);
-
-var omega_1 = 45.0;
-var omega_2 = 45.0;
-var omega_3 = 45.0;
-var omega_4 = 45.0;
+// ----------------------------------- DYNAMICS -----------------------------------
 
 var d = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var ddot = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 var dt = 0.001;
 
 droneDynamics();
 function droneDynamics() {
 	if (!paused) {
-		droneModelMatrix = createModelMatrix(d[0], d[1], d[2], d[3], d[4], d[5], 1.0, 1.0, 1.0);
-		let ddot = [
-			d[6],  // \dot x
-			d[7],  // \dot y
-			d[8],  // \dot z
-			d[9] + d[10] * Math.sin(d[3]) * Math.tan(d[4]) + d[11] * Math.cos(d[3]) * Math.tan(d[4]),  // \dot\theta'
-			d[10] * Math.cos(d[3]) - d[11] * Math.sin(d[3]),  // \dot\phi'
-			d[10] * Math.sin(d[3]) / Math.cos(d[4]) + d[11] * Math.cos(d[3]) / Math.cos(d[4]),  // \dot\psi'
+		let F1 = k_f * omega_1 * omega_1;
+		let F2 = k_f * omega_2 * omega_2;
+		let F3 = k_f * omega_3 * omega_3;
+		let F4 = k_f * omega_4 * omega_4;
 
-			// Linear accelerations
-			m ** (-1) * Math.sin(d[5]) * Math.cos(d[4]) * (k_f * (Math.pow(omega_1, 2) + Math.pow(omega_2, 2) + Math.pow(omega_3, 2) + Math.pow(omega_4, 2))),  // Along x
-			m ** (-1) * (Math.sin(d[4]) * Math.sin(d[5]) * Math.sin(d[3]) + Math.cos(d[5]) * Math.cos(d[3])) * (k_f * (Math.pow(omega_1, 2) + Math.pow(omega_2, 2) + Math.pow(omega_3, 2) + Math.pow(omega_4, 2))) - g,  // Along y
-			m ** (-1) * (Math.sin(d[4]) * Math.sin(d[5]) * Math.cos(d[3]) - Math.sin(d[3]) * Math.cos(d[5])) * (k_f * (Math.pow(omega_1, 2) + Math.pow(omega_2, 2) + Math.pow(omega_3, 2) + Math.pow(omega_4, 2))),  // Along z
+		let thrust = [0, F1 + F2 + F3 + F4, 0];
 
-			// Angular accelerations
-			i_x ** (-1) * (L * k_f * ((Math.pow(omega_3, 2) + Math.pow(omega_4, 2)) - (Math.pow(omega_2, 2) + Math.pow(omega_1, 2))) - d[11] * d[10] * (i_x - i_y)),  // \dot\theta'
-			i_y ** (-1) * (k_m * ((Math.pow(omega_1, 2) + Math.pow(omega_3, 2)) - (Math.pow(omega_2, 2) + Math.pow(omega_4, 2))) - d[11] * d[9] * (i_x - i_z)),  // \dot\phi'
-			i_z ** (-1) * (L * ((Math.pow(omega_2, 2) + Math.pow(omega_3, 2)) - (Math.pow(omega_1, 2) + Math.pow(omega_4, 2))) - d[9] * d[10] * (i_y - i_x))  // \dot\psi'
-		];
+
 
 		d = d.map((val, index) => val + dt * ddot[index]);
-
-		console.log("x: " + d[0].toFixed(1) +
-			" y: " + d[1].toFixed(1) +
-			" z: " + d[2].toFixed(1) +
-			" theta: " + d[3].toFixed(1) +
-			" phi: " + d[4].toFixed(1) +
-			" psi: " + d[5].toFixed(1));
-		console.log("omega_1: " + omega_1.toFixed(1) +
-			" omega_2: " + omega_2.toFixed(1) +
-			" omega_3: " + omega_3.toFixed(1) +
-			" omega_4: " + omega_4.toFixed(1));
 	}
 	setTimeout(droneDynamics, 1000);
 }
