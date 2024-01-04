07040fa319709be1287360ba632712bb0e065d9f
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-01-04 09:03:57 +0000

    Update drone dynamics variables and calculations

--- src/dynamics.js
+++ src/dynamics.js
@@ -24,12 +24,12 @@ setInterval(() => {
 
 setInterval(() => {
 	if (!paused) {
-		console.log("x: " + d[0].toFixed(1) +
-			" y: " + d[1].toFixed(1) +
-			" z: " + d[2].toFixed(1) +
-			" theta: " + d[3].toFixed(1) +
-			" phi: " + d[4].toFixed(1) +
-			" psi: " + d[5].toFixed(1));
+		console.log("x: " + X[0].toFixed(1) +
+			" y: " + X[1].toFixed(1) +
+			" z: " + X[2].toFixed(1) +
+			" theta: " + X[3].toFixed(1) +
+			" phi: " + X[4].toFixed(1) +
+			" psi: " + X[5].toFixed(1));
 		console.log("omega_1: " + omega_1.toFixed(1) +
 			" omega_2: " + omega_2.toFixed(1) +
 			" omega_3: " + omega_3.toFixed(1) +
@@ -56,23 +56,21 @@ setInterval(() => {
 	omega_3 = Math.min(Math.max(20, omega_3 + factoromega + factoromega_3), omega_max);
 	omega_4 = Math.min(Math.max(20, omega_4 + factoromega + factoromega_4), omega_max);
 	if (keys["r"]) {
-		d = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+		X = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 		omega_1 = 50.0;
 		omega_2 = 50.0;
 		omega_3 = 50.0;
 		omega_4 = 50.0;
 
 	}
-
-	droneModelMatrix = createModelMatrix(d[0], d[1], d[2], d[3], d[4], d[5], 1.0, 1.0, 1.0);
+	droneModelMatrix = createModelMatrix(X[0], X[1], X[2], X[3], X[4], X[5], 1.0, 1.0, 1.0);
 
 }, 10);
 
 
 // ----------------------------------- DYNAMICS -----------------------------------
-
-var d = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-var ddot = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var X = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var Xdot = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 var dt = 0.001;
 
 droneDynamics();
@@ -83,11 +81,23 @@ function droneDynamics() {
 		let F3 = k_f * omega_3 * omega_3;
 		let F4 = k_f * omega_4 * omega_4;
 
-		let thrust = [0, F1 + F2 + F3 + F4, 0];
+		let M1 = k_m * omega_1 * omega_1;
+		let M2 = k_m * omega_2 * omega_2;
+		let M3 = k_m * omega_3 * omega_3;
+		let M4 = k_m * omega_4 * omega_4;
+
+		let thrust = [0, F1 + F2 + F3 + F4, 0, 1];
+		thrust = mult(createXRotationMatrix(degreeToRadians(X[3])), thrust);
+		thrust = mult(createYRotationMatrix(degreeToRadians(X[4])), thrust);
+		thrust = mult(createZRotationMatrix(degreeToRadians(X[5])), thrust);
+
+		let acc = [thrust[0] / m, thrust[1] / m - g, thrust[2] / m, 1];
+
+		let torque = [L * ((F3 + F4) - (F2 + F1)), (M1 + M3) - (M2 + M4), L * ((F2 + F3) - (F1 + F4)), 1];
 
 
 
-		d = d.map((val, index) => val + dt * ddot[index]);
+		X = X.map((val, index) => val + dt * Xdot[index]);
 	}
 	setTimeout(droneDynamics, 1000);
 }
