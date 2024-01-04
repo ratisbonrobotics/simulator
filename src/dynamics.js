38cb3f27b18658156c13d30d0aff867d0bcaf7ca
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-01-04 08:19:05 +0000

    Refactor dynamics.js: Reorder code and update initial values

--- src/dynamics.js
+++ src/dynamics.js
@@ -1,26 +1,4 @@
-// ----------------------------------- LOGIC -----------------------------------
-droneModelMatrix = createModelMatrix(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0);
-terrainModelMatrix = createModelMatrix(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0);
-
-const k_f = 0.00141446535;
-const k_m = 0.0004215641;
-const m = 1.0;
-const L = 0.23;
-const i_x = 0.0121;
-const i_y = 0.0119;
-const i_z = 0.0223;
-const g = 9.81;
-
-const omega_min = 20
-const omega_max = 66
-
-var omega_1 = 46.0;
-var omega_2 = 45.0;
-var omega_3 = 45.0;
-var omega_4 = 45.0;
-
-var d = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-var dt = 0.001;
+// ----------------------------------- ENV + UTIL -----------------------------------
 
 var paused = false;
 
@@ -61,6 +39,28 @@ setInterval(() => {
 
 }, 10);
 
+const k_f = 0.00141446535;
+const k_m = 0.0004215641;
+const m = 1.0;
+const L = 0.23;
+const i_x = 0.0121;
+const i_y = 0.0119;
+const i_z = 0.0223;
+const g = 9.81;
+const omega_min = 20
+const omega_max = 66
+
+// ----------------------------------- LOGIC -----------------------------------
+droneModelMatrix = createModelMatrix(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0);
+terrainModelMatrix = createModelMatrix(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0);
+
+var omega_1 = 45.0;
+var omega_2 = 45.0;
+var omega_3 = 45.0;
+var omega_4 = 45.0;
+
+var d = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var dt = 0.001;
 
 droneDynamics();
 function droneDynamics() {
