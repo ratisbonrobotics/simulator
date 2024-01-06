8a9e10e08c69ac9e108fa36ed1a3d9da0d9b380d
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-01-06 15:09:05 +0000

    Fix formatting and update comments in aerodynamics.html and matrix4f.js, and adjust calculations in dynamics.js

--- src/dynamics.js
+++ src/dynamics.js
@@ -68,12 +68,15 @@ function droneDynamics() {
 		let M3 = k_m * omega_3 * omega_3;
 		let M4 = k_m * omega_4 * omega_4;
 
-		let local_thrust = [0, F1 + F2 + F3 + F4, 0];
 		let R = transposeMat3f(multMat3f(xRotMat3f(X["phi"]), multMat3f(yRotMat3f(X["theta"]), zRotMat3f(X["psi"]))));
-		let global_thrust = multMatVec3f(R, local_thrust);
+		console.log(R[6], Math.sin(X["theta"]));
+		let global_thrust = multMatVec3f(R, [0, F1 + F2 + F3 + F4, 0]);
 		let global_linear_accelerations = [global_thrust[0] / m, global_thrust[1] / m - g, global_thrust[2] / m];
 
-		let torque = [L * ((F3 + F4) - (F2 + F1)), (M1 + M3) - (M2 + M4), L * ((F2 + F3) - (F1 + F4))];
+		let torque = [
+			L * ((F3 + F4) - (F2 + F1)),
+			(M1 + M3) - (M2 + M4),
+			L * ((F2 + F3) - (F1 + F4))];
 
 		let local_rotational_velocities = [X["phi_hat_dot"], X["theta_hat_dot"], X["psi_hat_dot"]];
 		let local_inerta_matrix = vecToDiagMat3f([i_phi_hat, i_theta_hat, i_psi_hat]);
@@ -103,7 +106,7 @@ function droneDynamics() {
 			X[key] += X_dot[key + "_dot"] * dt;
 		}
 
-		droneModelMatrix = modelMat4f(X["x"], X["y"], X["z"], X["phi"], X["theta"], X["psi"], 1.0, 1.0, 1.0);
+		//droneModelMatrix = modelMat4f(X["x"], X["y"], X["z"], X["phi"], X["theta"], X["psi"], 1.0, 1.0, 1.0);
 
 		if (X["y"] < 0.0 || X["x"] > 1.0 || X["x"] < -1.0 || X["z"] > 1.0 || X["z"] < -1.0) {
 			reset();
