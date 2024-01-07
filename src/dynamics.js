d1e90aa91fd6d5e9496a0ce0fdcd3401996673e6
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-01-07 17:25:12 +0000

    Refactor dynamics calculations in dynamics.js

--- src/dynamics.js
+++ src/dynamics.js
@@ -64,63 +64,15 @@ setInterval(function () {
 	let M3 = k_m * omega_3 * omega_3;
 	let M4 = k_m * omega_4 * omega_4;
 
-	let R = xRotMat3f(X["phi"]);
-	R = multMat3f(R, yRotMat3f(X["theta"]));
-	R = multMat3f(R, zRotMat3f(X["psi"]));
-	R = transposeMat3f(R);
+	let R = transposeMat3f(multMat3f(xRotMat3f(X["phi"]), multMat3f(yRotMat3f(X["theta"]), zRotMat3f(X["psi"]))));
 
-	let global_thrust = multMatVec3f(R, [0, (F1 + F2 + F3 + F4), 0]);
+	let global_thrust = multMatVec3f(R, [0, F1 + F2 + F3 + F4, 0]);
 	let global_linear_accelerations = [global_thrust[0] / m, global_thrust[1] / m - g, global_thrust[2] / m];
 
-	// Assuming L, F1, F2, F3, F4, M1, M2, M3, M4, i_phi_hat, i_theta_hat, i_psi_hat are defined elsewhere
-
-	// Step 1: Define the torque vector
-	let torque = [
-		L * ((F3 + F4) - (F2 + F1)),
-		(M1 + M3) - (M2 + M4),
-		L * ((F2 + F3) - (F1 + F4))
-	];
-
-	// Step 2: Define local rotational velocities
+	let torque = [L * ((F3 + F4) - (F2 + F1)), (M1 + M3) - (M2 + M4), L * ((F2 + F3) - (F1 + F4))];
 	let local_rotational_velocities = [X["phi_hat_dot"], X["theta_hat_dot"], X["psi_hat_dot"]];
-
-	// Step 3: Convert local inertia values to a diagonal inertia matrix
-	function vecToDiagMat3f(vector) {
-		return [
-			[vector[0], 0, 0],
-			[0, vector[1], 0],
-			[0, 0, vector[2]]
-		];
-	}
-	let local_inertia_matrix = vecToDiagMat3f([i_phi_hat, i_theta_hat, i_psi_hat]);
-
-	// Step 4: Calculate the cross product term
-	function crossProduct(v1, v2) {
-		return [
-			v1[1] * v2[2] - v1[2] * v2[1],
-			v1[2] * v2[0] - v1[0] * v2[2],
-			v1[0] * v2[1] - v1[1] * v2[0]
-		];
-	}
-
-	function matrixVectorMultiply(matrix, vector) {
-		return matrix.map(row => row.reduce((sum, value, index) => sum + value * vector[index], 0));
-	}
-
-	let omega = local_rotational_velocities;
-	let inertiaOmega = matrixVectorMultiply(local_inertia_matrix, omega);
-	let cross_product_term = crossProduct(omega, inertiaOmega);
-
-	// Step 5: Calculate local rotational accelerations
-	function inverseDiagonalMatrix(matrix) {
-		return matrix.map((row, index) => row.map((value, colIndex) => index === colIndex ? 1 / value : 0));
-	}
-
-	let inverse_inertia = inverseDiagonalMatrix(local_inertia_matrix);
-	let local_rotational_accelerations = matrixVectorMultiply(
-		inverse_inertia,
-		torque.map((t, i) => t - cross_product_term[i])
-	);
+	let local_inerta_matrix = vecToDiagMat3f([i_phi_hat, i_theta_hat, i_psi_hat]);
+	let local_rotational_accelerations = multMatVec3f(invMat3f(local_inerta_matrix), subVec3f(torque, crossVec3f(local_rotational_velocities, multMatVec3f(local_inerta_matrix, local_rotational_velocities))));
 
 	let global_rotational_velocities = multMatVec3f(R, local_rotational_velocities);
 
@@ -140,11 +92,9 @@ setInterval(function () {
 	X_dot["theta_hat_dot_dot"] = local_rotational_accelerations[1];
 	X_dot["psi_hat_dot_dot"] = local_rotational_accelerations[2];
 
-	Object.keys(X).forEach(key => {
-		if (X_dot.hasOwnProperty(key + "_dot")) {
-			X[key] += X_dot[key + "_dot"] * dt;
-		}
-	});
+	for (key in X) {
+		X[key] += X_dot[key + "_dot"] * dt;
+	}
 
 	droneModelMatrix = modelMat4f(X["x"], X["y"], X["z"], X["phi"], X["theta"], X["psi"], 0.01, 0.01, 0.01);
 
