f0ff3d0833cd827f6408f799f26d42d1adef3a60
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-01-08 15:29:38 +0000

    Update drone dynamics calculations

--- src/dynamics.js
+++ src/dynamics.js
@@ -3,24 +3,21 @@ const k_f = 0.00141446535;
 const k_m = 0.0004215641;
 const L = 0.23;
 const l = (L / Math.sqrt(2));
-const I_hat = vecToDiagMat3f([0.0121, 0.0223, 0.0119]);
-const inv_I_hat = invMat3f(I_hat);
+const I = vecToDiagMat3f([0.0121, 0.0223, 0.0119]);
 const g = 9.81;
 const m = 1.0;
 const omega_min = 20
 const omega_max = 66
 
 // ----------------------------------- DYNAMICS -----------------------------------
-var omega_1 = 41.66;
+var omega_1 = 41.67;
 var omega_2 = 41.65;
 var omega_3 = 41.65;
 var omega_4 = 41.65;
 
-var loc_rot_vel = [0.0, 0.0, 0.0];
-var loc_rot_acc = [0.0, 0.0, 0.0];
-var glob_lin_vel = [0.0, 0.0, 0.0];
-
+var glob_rot_vel = [0.0, 0.0, 0.0];
 var glob_rot_pos = [0.0, 0.0, 0.0];
+var glob_lin_vel = [0.0, 0.0, 0.0];
 var glob_lin_pos = [0.0, 0.2, 0.0];
 
 setInterval(function () {
@@ -42,7 +39,7 @@ setInterval(function () {
 	glob_lin_pos = addVec3f(glob_lin_pos, multScalVec3f(dt, glob_lin_vel));
 
 	// --- TORQUE AND ROTATION ---
-	let torque = [0, 0, 0];
+	let loc_torque = [0, 0, 0];
 	let tau_1f = crossVec3f([-l, 0, l], [0, F1, 0]);
 	let tau_1m = crossVec3f([-l, 0, l], [M1, 0, 0]);
 	let tau_2f = crossVec3f([l, 0, l], [0, F2, 0]);
@@ -51,19 +48,24 @@ setInterval(function () {
 	let tau_3m = crossVec3f([l, 0, -l], [M3, 0, 0]);
 	let tau_4f = crossVec3f([-l, 0, -l], [0, F4, 0]);
 	let tau_4m = crossVec3f([-l, 0, -l], [-M4, 0, 0]);
-	torque = addVec3f(torque, tau_1f);
-	torque = addVec3f(torque, tau_1m);
-	torque = addVec3f(torque, tau_2f);
-	torque = addVec3f(torque, tau_2m);
-	torque = addVec3f(torque, tau_3f);
-	torque = addVec3f(torque, tau_3m);
-	torque = addVec3f(torque, tau_4f);
-	torque = addVec3f(torque, tau_4m);
-	torque = multMatVec3f(R, torque);
+	loc_torque = addVec3f(loc_torque, tau_1f);
+	loc_torque = addVec3f(loc_torque, tau_1m);
+	loc_torque = addVec3f(loc_torque, tau_2f);
+	loc_torque = addVec3f(loc_torque, tau_2m);
+	loc_torque = addVec3f(loc_torque, tau_3f);
+	loc_torque = addVec3f(loc_torque, tau_3m);
+	loc_torque = addVec3f(loc_torque, tau_4f);
+	loc_torque = addVec3f(loc_torque, tau_4m);
+	let glob_torque = multMatVec3f(R, loc_torque);
 
-	loc_rot_acc = addVec3f(loc_rot_vel, multScalVec3f(dt, addVec3f(loc_rot_vel, multScalVec3f(dt, multMatVec3f(inv_I_hat, subVec3f(torque, crossVec3f(loc_rot_vel, multMatVec3f(I_hat, loc_rot_vel))))))));
-	loc_rot_vel = addVec3f(loc_rot_vel, multScalVec3f(dt, loc_rot_acc));
-	glob_rot_pos = addVec3f(glob_rot_pos, multScalVec3f(dt, multMatVec3f(R, loc_rot_vel)));
+
+	//let glob_I = multMatVec3f(R, loc_I);
+	let glob_I_mat = vecToDiagMat3f(loc_I);
+	let glob_I_mat_inv = invMat3f(glob_I_mat);
+
+	let glob_rot_acc = addVec3f(glob_rot_vel, multScalVec3f(dt, addVec3f(glob_rot_vel, multScalVec3f(dt, multMatVec3f(glob_I_mat_inv, subVec3f(glob_torque, crossVec3f(glob_rot_vel, multMatVec3f(glob_I_mat, glob_rot_vel))))))));
+	glob_rot_vel = addVec3f(glob_rot_vel, multScalVec3f(dt, glob_rot_acc));
+	glob_rot_pos = addVec3f(glob_rot_pos, multScalVec3f(dt, glob_rot_vel));
 
 	// --- UPDATE MODEL MATRIX ---
 	droneModelMatrix = modelMat4f(glob_lin_pos[0], glob_lin_pos[1], glob_lin_pos[2], glob_rot_pos[0], glob_rot_pos[1], glob_rot_pos[2], 0.01, 0.01, 0.01);
@@ -71,12 +73,11 @@ setInterval(function () {
 	console.log(Math.floor(glob_lin_pos[0] * 100) / 100, Math.floor(glob_lin_pos[1] * 100) / 100, Math.floor(glob_lin_pos[2] * 100) / 100);
 
 	if (glob_lin_pos[0] > 1 || glob_lin_pos[0] < -1 || glob_lin_pos[1] > 1 || glob_lin_pos[1] < -1 || glob_lin_pos[2] > 1 || glob_lin_pos[2] < -1) {
-		loc_rot_vel = [0.0, 0.0, 0.0];
-		loc_rot_acc = [0.0, 0.0, 0.0];
+		glob_rot_vel = [0.0, 0.0, 0.0];
+		glob_rot_pos = [0.0, 0.0, 0.0];
 		glob_lin_vel = [0.0, 0.0, 0.0];
 		glob_lin_pos = [0.0, 0.2, 0.0];
-		glob_rot_pos = [0.0, 0.0, 0.0];
 	}
 
 
-}, dt * 1000);
+}, dt * 1000/*dt * 1000*/);
