6bf02b85db5185a6c75d80336938349406c35a19
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-01-08 10:54:58 +0000

    Add console.log for drone position

--- src/dynamics.js
+++ src/dynamics.js
@@ -46,6 +46,8 @@ setInterval(function () {
 	// --- UPDATE MODEL MATRIX ---
 	droneModelMatrix = modelMat4f(glob_lin_pos[0], glob_lin_pos[1], glob_lin_pos[2], glob_rot_pos[0], glob_rot_pos[1], glob_rot_pos[2], 0.01, 0.01, 0.01);
 
+	console.log(Math.floor(glob_lin_pos[0] * 100) / 100, Math.floor(glob_lin_pos[1] * 100) / 100, Math.floor(glob_lin_pos[2] * 100) / 100);
+
 	if (glob_lin_pos[0] > 1 || glob_lin_pos[0] < -1 || glob_lin_pos[1] > 1 || glob_lin_pos[1] < -1 || glob_lin_pos[2] > 1 || glob_lin_pos[2] < -1) {
 		loc_rot_vel = [0.0, 0.0, 0.0];
 		glob_lin_vel = [0.0, 0.0, 0.0];
