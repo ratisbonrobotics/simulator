0d49ccd8a08de895f442397793e49121d5dfd0ce
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-01-09 15:23:05 +0000

    Update dynamics.js with corrected torque calculation

--- src/dynamics.js
+++ src/dynamics.js
@@ -12,7 +12,7 @@ const omega_min = 20
 const omega_max = 66
 
 // ----------------------------------- DYNAMICS -----------------------------------
-var omega_1 = 41.65;
+var omega_1 = 41.66;
 var omega_2 = 41.65;
 var omega_3 = 41.65;
 var omega_4 = 41.65;
@@ -43,9 +43,7 @@ setInterval(function () {
 	glob_lin_pos = addVec3f(glob_lin_pos, multScalVec3f(dt, glob_lin_vel));
 
 	// --- TORQUE AND ROTATION ---
-	let loc_torque = [l * (F3 + F4 - F2 - F1), (M1 - M2 + M3 - M4), l * (F2 + F3 - F1 - F4)];
-	loc_torque = multScalVec3f(-1, loc_torque);
-
+	let loc_torque = [-l * (F3 + F4 - F2 - F1), -(M1 - M2 + M3 - M4), -l * (F2 + F3 - F1 - F4)];
 	let loc_rot_acc = multMatVec3f(loc_I_mat_inv, subVec3f(loc_torque, crossVec3f(loc_rot_vel, multMatVec3f(loc_I_mat, loc_rot_vel))));
 	loc_rot_vel = addVec3f(loc_rot_vel, multScalVec3f(dt, loc_rot_acc));
 
