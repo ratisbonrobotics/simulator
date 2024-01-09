0f1cdbb5f6b72f822c29ad7207cd3caa66bd8446
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-01-09 16:27:25 +0000

    Update drone dynamics calculations

--- src/dynamics.js
+++ src/dynamics.js
@@ -37,7 +37,8 @@ setInterval(function () {
 	let R_T = transpMat3f(R);
 
 	// --- THRUST AND POSITION ---
-	let glob_lin_acc = addVec3f(multScalVec3f(1 / m, multMatVec3f(R_T, [0, (F1 + F2 + F3 + F4), 0])), [0, -g, 0]);
+	let loc_lin_acc = [0, (1 / m) * (F1 + F2 + F3 + F4), 0];
+	let glob_lin_acc = addVec3f(multMatVec3f(R_T, loc_lin_acc), [0, -g, 0]);
 	glob_lin_vel = addVec3f(glob_lin_vel, multScalVec3f(dt, glob_lin_acc));
 	glob_lin_pos = addVec3f(glob_lin_pos, multScalVec3f(dt, glob_lin_vel));
 
