aa5678a6ca5285247075155c624f842226966dd0
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-04-16 12:02:51 +0000

    🛠️ simulation/model/control.js -> Removed altitude control 🛠️ simulation/model/control.js -> Updated yaw control values 🛠️ simulation/model/control.js -> Simplified motor commands

--- simulation/model/control.js
+++ simulation/model/control.js
@@ -5,9 +5,6 @@ const Kd_pitch = 0.01;
 const Kp_yaw = 0.1;
 const Kd_yaw = 0.01;
 
-const Kp_alt = 3.0;
-const Kd_alt = 1.8;
-
 var desired_loc_rot_pos = [0.0, 0.0, 0.0];
 var desired_loc_rot_vel = [0.0, 0.0, 0.0];
 var desired_loc_rot_acc = [0.0, 0.0, 0.0];
@@ -32,21 +29,12 @@ setInterval(function () {
     }
 
     // --- YAW INPUT ---
-    if (keys["q"]) {
-        desired_loc_rot_vel[1] = 0.001; // Turn left
-    } else if (keys["e"]) {
-        desired_loc_rot_vel[1] = -0.001; // Turn right
+    if (keys["e"]) {
+        desired_loc_rot_vel[1] = 0.1; // Turn left
+    } else if (keys["q"]) {
+        desired_loc_rot_vel[1] = -0.1; // Turn right
     } else {
         desired_loc_rot_vel[1] = 0.0;
-    }
-
-    // --- ALTITUDE INPUT ---
-    if (keys["x"]) {
-        desired_loc_lin_vel[1] = 0.1; // Up
-    } else if (keys["z"]) {
-        desired_loc_lin_vel[1] = -0.1; // Down
-    } else {
-        desired_loc_lin_vel[1] = 0.0;
     }
 
     // --- CONTROL ---
@@ -54,13 +42,10 @@ setInterval(function () {
     pitch_control = 0;
     let yaw_control = Kp_yaw * (desired_loc_rot_vel[1] - loc_rot_vel[1]) + Kd_yaw * (desired_loc_rot_acc[1] - loc_rot_vel[1]);
 
-    let alt_control = Kp_alt * (desired_loc_lin_vel[1] - loc_lin_vel[1]) + Kd_alt * (desired_loc_lin_acc[1] - loc_lin_acc[1]);
-    alt_control = 0;
-
     // --- MOTOR COMMANDS ---
-    omega_1 = omega_stable + yaw_control - pitch_control + alt_control;
-    omega_2 = omega_stable - yaw_control - pitch_control + alt_control;
-    omega_3 = omega_stable + yaw_control + pitch_control + alt_control;
-    omega_4 = omega_stable - yaw_control + pitch_control + alt_control;
+    omega_1 = omega_stable - yaw_control - pitch_control;
+    omega_2 = omega_stable + yaw_control - pitch_control;
+    omega_3 = omega_stable - yaw_control + pitch_control;
+    omega_4 = omega_stable + yaw_control + pitch_control;
 
 }, dt * 10);
