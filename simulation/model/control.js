b89c92868c74ae503a0a11788017411e51f8e32c
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-04-16 10:19:27 +0000

    🛠️ simulation/model/control.js -> Increased rotation sensitivity, adjusted motor control logic

--- simulation/model/control.js
+++ simulation/model/control.js
@@ -14,25 +14,25 @@ var desired_alt = 0.2;
 setInterval(function () {
     // --- USER INPUT ---
     if (keys["w"]) {
-        desired_rot_vel[0] = -0.001; // Pitch forward
+        desired_rot_vel[0] = -0.1; // Pitch forward
     } else if (keys["s"]) {
-        desired_rot_vel[0] = 0.001; // Pitch backward
+        desired_rot_vel[0] = 0.1; // Pitch backward
     } else {
         desired_rot_vel[0] = 0.0;
     }
 
     if (keys["a"]) {
-        desired_rot_vel[2] = -0.001; // Roll left
+        desired_rot_vel[2] = -0.1; // Roll left
     } else if (keys["d"]) {
-        desired_rot_vel[2] = 0.001; // Roll right
+        desired_rot_vel[2] = 0.1; // Roll right
     } else {
         desired_rot_vel[2] = 0.0;
     }
 
     if (keys["q"]) {
-        desired_rot_vel[1] = -0.001; // Yaw left (counterclockwise)
+        desired_rot_vel[1] = -0.1; // Yaw left (counterclockwise)
     } else if (keys["e"]) {
-        desired_rot_vel[1] = 0.001; // Yaw right (clockwise)
+        desired_rot_vel[1] = 0.1; // Yaw right (clockwise)
     } else {
         desired_rot_vel[1] = 0.0;
     }
@@ -66,10 +66,10 @@ setInterval(function () {
     omega_3 = omega_stable - roll_control - pitch_control - yaw_control + alt_control;
     omega_4 = omega_stable + roll_control - pitch_control + yaw_control + alt_control;
 */
-    omega_1 = omega_stable + alt_control;
-    omega_2 = omega_stable + alt_control;
-    omega_3 = omega_stable + alt_control;
-    omega_4 = omega_stable + alt_control;
+    omega_1 = omega_stable - yaw_control + alt_control;
+    omega_2 = omega_stable + yaw_control + alt_control;
+    omega_3 = omega_stable - yaw_control + alt_control;
+    omega_4 = omega_stable + yaw_control + alt_control;
 
     // --- LIMIT MOTOR SPEEDS ---
     omega_1 = Math.max(Math.min(omega_1, omega_max), omega_min);
