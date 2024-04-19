29dbf9edfebaa3e47085e7eeb3224d534abc4081
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-04-19 10:50:22 +0000

    🛠️ lib/matrix4f.js -> Removed setXYZ, setRotationMatrixOfModelMatrix; added multMatVec4f.
    🛠️ lib/vector3f.js -> Changed 'var' to 'let' in normVec3f.
    
    🛠️ simulation/model/control.js -> Updated var to let; changed comment to 'ATTITUDE CONTROL'; added ROTOR SPEEDS section.

--- simulation/model/control.js
+++ simulation/model/control.js
@@ -1,10 +1,10 @@
 // ----------------------------------- CONTROL PARAMETERS -----------------------------------
-var linear_position_d_W = [1, 1, 1];
-var linear_velocity_d_W = [0, 0, 0];
-var linear_acceleration_d_W = [0, 0, 0];
-var angular_velocity_d_B = [0, 0, 0];
-var angular_acceleration_d_B = [0, 0, 0];
-var yaw_d = 3.14;
+let linear_position_d_W = [1, 1, 1];
+let linear_velocity_d_W = [0, 0, 0];
+let linear_acceleration_d_W = [0, 0, 0];
+let angular_velocity_d_B = [0, 0, 0];
+let angular_acceleration_d_B = [0, 0, 0];
+let yaw_d = 3.14;
 
 const k_p = 1.0;
 const k_v = 1.0;
@@ -24,7 +24,7 @@ setInterval(function () {
     let z_W_B = multMatVec3f(R_W_B, [0, 1, 0]);
     let f_z_B_control = dotVec3f(z_W_d, z_W_B);
 
-    // --- ANGULAR CONTROL ---
+    // --- ATTITIDUE CONTROL ---
     let x_tilde_d_W = [Math.cos(yaw_d), 0, Math.sin(yaw_d)];
     let R_W_d_column_2 = normVec3f(z_W_d);
     let R_W_d_column_1 = normVec3f(crossVec3f(z_W_d, x_tilde_d_W));
@@ -44,5 +44,24 @@ setInterval(function () {
     let term_0 = multMat3f(transpMat3f(R_W_B), multMatVec3f(R_W_d, angular_acceleration_d_B));
     let term_1 = crossVec3f(angular_velocity_B, multMat3f(transpMat3f(R_W_B), multMatVec3f(R_W_d, angular_velocity_d_B)));
     tau_B_control = subVec3f(tau_B_control, multMatVec3f(loc_I_mat, subVec3f(term_1, term_0)));
+
+    // --- ROTOR SPEEDS ---
+    let F_bar_column_0 = addVec3f([0, k_m, 0], crossVec3f(multScalVec3f(k_f, [-L, 0, L]), [0, 1, 0]));
+    let F_bar_column_1 = addVec3f([0, -k_m, 0], crossVec3f(multScalVec3f(k_f, [L, 0, L]), [0, 1, 0]));
+    let F_bar_column_2 = addVec3f([0, k_m, 0], crossVec3f(multScalVec3f(k_f, [-L, 0, -L]), [0, 1, 0]));
+    let F_bar_column_3 = addVec3f([0, -k_m, 0], crossVec3f(multScalVec3f(k_f, [-L, 0, -L]), [0, 1, 0]));
+    let F_bar = [
+        k_f, k_f, k_f, k_f,
+        F_bar_column_0[0], F_bar_column_1[0], F_bar_column_2[0], F_bar_column_3[0],
+        F_bar_column_0[1], F_bar_column_1[1], F_bar_column_2[1], F_bar_column_3[1],
+        F_bar_column_0[2], F_bar_column_1[2], F_bar_column_2[2], F_bar_column_3[2]
+    ];
+    let F_bar_inv = inv4Mat4f(F_bar);
+    let omega_sign_square = multMatVec4f(F_bar_inv, [f_z_B_control, tau_B_control[0], tau_B_control[1], tau_B_control[2]]);
+
+    omega_1 = Math.sqrt(omega_sign_square[0]);
+    omega_2 = Math.sqrt(omega_sign_square[1]);
+    omega_3 = Math.sqrt(omega_sign_square[2]);
+    omega_4 = Math.sqrt(omega_sign_square[3]);
 
 }, dt * 10);
