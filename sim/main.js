261fd1e52f69b6ec874f3daa96df8747a042e89c
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-04-24 11:11:20 +0000

    🛠️ sim/main.js -> Added vertex normals, improved lighting

--- sim/main.js
+++ sim/main.js
@@ -3,15 +3,18 @@ const vertexshadersource = `
     precision highp float;
 
     attribute vec4 vertexposition;
+    attribute vec3 vertexnormal;
     attribute vec2 texturecoordinate;
     uniform mat4 modelmatrix;
     uniform mat4 viewmatrix;
     uniform mat4 projectionmatrix;
 
     varying vec2 o_texturecoordinate;
-
-    void main(){
+    varying vec3 o_vertexnormal;
+    
+    void main() {
         o_texturecoordinate = texturecoordinate;
+        o_vertexnormal = vertexnormal;
         gl_Position = projectionmatrix * viewmatrix * modelmatrix * vertexposition;
     }
 `;
@@ -20,19 +23,25 @@ const fragmentshadersource = `
     precision highp float;
 
     uniform sampler2D texture;
-
     varying vec2 o_texturecoordinate;
+    varying vec3 o_vertexnormal;
 
-    void main(){
-        gl_FragColor = texture2D(texture, o_texturecoordinate);
+    void main() {
+        vec3 normalizedNormal = normalize(o_vertexnormal);
+        vec3 lightDirection = normalize(vec3(1.0, 1.0, 1.0));
+        float lightIntensity = max(dot(normalizedNormal, lightDirection), 0.0);
+        vec4 textureColor = texture2D(texture, o_texturecoordinate);
+        
+        gl_FragColor = vec4(textureColor.rgb * lightIntensity, textureColor.a);
     }
 `;
+
 
 // --- MAKE SHADERS AND PROGRAM ---
 const program = createAndUseProgram(gl, vertexshadersource, fragmentshadersource);
 
 // --- GET ATTRIBUTE AND UNIFORM LOCATIONS ---
-const attribLocations = getAttribLocations(gl, program, ["vertexposition", "texturecoordinate"]);
+const attribLocations = getAttribLocations(gl, program, ["vertexposition", "texturecoordinate", "vertexnormal"]);
 const uniformLocations = getUniformLocations(gl, program, ["modelmatrix", "viewmatrix", "projectionmatrix", "texture"]);
 
 // --- INIT 3D ---
@@ -40,6 +49,7 @@ init3D(gl);
 
 // --- GET DATA FROM 3D FILES ---
 let scene_vertexbuffer = [];
+let scene_normalbuffer = [];
 let scene_texcoordbuffer = [];
 let scene_texture = [];
 
@@ -51,6 +61,7 @@ async function loadScene() {
         scene_vertexbuffer[k][0] = createBuffer(gl, gl.ARRAY_BUFFER, value["v"]);
         scene_vertexbuffer[k][1] = Math.floor(value["v"].length / 3);
         scene_texcoordbuffer[k] = createBuffer(gl, gl.ARRAY_BUFFER, value["vt"]);
+        scene_normalbuffer[k] = createBuffer(gl, gl.ARRAY_BUFFER, value["vn"]);
 
         if (value["m"][0]["map_Kd"] && value["m"][0]["map_Kd"].src) {
             scene_texture[k] = addTexture(gl, value["m"][0]["map_Kd"].src);
@@ -65,12 +76,14 @@ async function loadScene() {
 
 let drone_vertexbuffer;
 let drone_texcoordbuffer;
+let drone_normalbuffer;
 let drone_texture;
 loadDrone();
 async function loadDrone() {
     let [obj, mtl] = await parseOBJ('/sim/data/drone.obj');
     drone_vertexbuffer = createBuffer(gl, gl.ARRAY_BUFFER, obj["drone"]["v"]);
     drone_texcoordbuffer = createBuffer(gl, gl.ARRAY_BUFFER, obj["drone"]["vt"]);
+    drone_normalbuffer = createBuffer(gl, gl.ARRAY_BUFFER, obj["drone"]["vn"]);
     drone_texture = addTexture(gl, mtl["Material"]["map_Kd"].src);
     await loadScene();
     requestAnimationFrame(drawScene);
@@ -95,6 +108,7 @@ function drawScene() {
     for (let primitive = 0; primitive < scene_vertexbuffer.length; primitive++) {
         connectBufferToAttribute(gl, gl.ARRAY_BUFFER, scene_vertexbuffer[primitive][0], attribLocations.vertexposition, 3);
         connectBufferToAttribute(gl, gl.ARRAY_BUFFER, scene_texcoordbuffer[primitive], attribLocations.texturecoordinate, 2);
+        connectBufferToAttribute(gl, gl.ARRAY_BUFFER, scene_normalbuffer[primitive], attribLocations.vertexnormal, 3);
         gl.uniform1i(uniformLocations["texture"], scene_texture[primitive]);
         gl.uniformMatrix4fv(uniformLocations["modelmatrix"], false, sceneModelMatrix);
         gl.drawArrays(gl.TRIANGLES, 0, scene_vertexbuffer[primitive][1]);
@@ -103,6 +117,7 @@ function drawScene() {
     // --- DRAW DRONE ---
     connectBufferToAttribute(gl, gl.ARRAY_BUFFER, drone_vertexbuffer, attribLocations.vertexposition, 3);
     connectBufferToAttribute(gl, gl.ARRAY_BUFFER, drone_texcoordbuffer, attribLocations.texturecoordinate, 2);
+    connectBufferToAttribute(gl, gl.ARRAY_BUFFER, drone_normalbuffer, attribLocations.vertexnormal, 3);
     gl.uniform1i(uniformLocations["texture"], drone_texture);
     gl.uniformMatrix4fv(uniformLocations["modelmatrix"], false, droneModelMatrix);
     gl.drawArrays(gl.TRIANGLES, 0, 924);
