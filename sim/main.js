7055ebe1ffadd4c8bfcf9b5ed3d748736e897012
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-04-29 13:08:44 +0000

    🛠️ index.html -> Removed reference to env.js 🔴 sim/env.js -> Deleted file 🛠️ sim/main.js -> Integrated env.js setup and logic

--- sim/main.js
+++ sim/main.js
@@ -1,3 +1,19 @@
+// --- SETUP ENVIRONMENT ---
+let keys = {};
+let mouse = { horizontal: 0, vertical: 0 }
+const canvas = document.getElementById("canvas");
+const gl = canvas.getContext('webgl', { preserveDrawingBuffer: true });
+init3D(gl);
+resizeCanvas(canvas);
+
+// --- ADD EVENT LISTENERS ---
+window.addEventListener("orientationchange", function () { resizeCanvas(canvas); });
+window.addEventListener("resize", function () { resizeCanvas(canvas); });
+canvas.addEventListener("mousemove", function (event) { if (document.pointerLockElement === canvas) { mouse["horizontal"] = event.movementX; mouse["vertical"] = event.movementY; } });
+canvas.addEventListener("click", canvas.requestPointerLock);
+canvas.addEventListener("keydown", function (event) { keys[event.key] = true; });
+canvas.addEventListener("keyup", function (event) { keys[event.key] = false; });
+
 // --- CREATE SHADOW FRAMEBUFFERS, TEXTURES AND LIGHT PROJECTION MATRICES ---
 const lights = { "res": 2048, "num": 4, "framebuf": [], "tex": [], "proj": [], "pos": [[-1, 2.9, -5.3], [0, 2.9, 0], [-3, 2.9, -3], [-5, 2.9, 0]], "look": [[-1, 0, -5], [0, 0, 0.3], [-3, 0, -3.3], [-5.3, 0, 0]] }
 
@@ -53,4 +69,22 @@ function renderScene() {
 
     drawDrawable(gl, sceneDrawable, attribLocations, uniformLocations, true);
     drawDrawable(gl, droneDrawable, attribLocations, uniformLocations, true);
+}
+
+// --- GET DATA FROM 3D FILES ---
+let sceneDrawable = { "vertexbuffer": [], "normalbuffer": [], "texcoordbuffer": [], "texture": [], "modelmatrix": modelMat4f(2.0, 0.0, 2.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0) };
+let droneDrawable = { "vertexbuffer": [], "normalbuffer": [], "texcoordbuffer": [], "texture": [], "modelmatrix": modelMat4f(0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.01, 0.01, 0.01) };
+
+loadData();
+async function loadData() {
+    await loadDrawable('/sim/data/drone.obj', droneDrawable);
+    await loadDrawable('/sim/data/scene.obj.gz', sceneDrawable);
+    drawScene();
+}
+
+// --- MAIN LOOP ---
+function drawScene() {
+    renderDepthMap();
+    renderScene();
+    requestAnimationFrame(drawScene);
 }
