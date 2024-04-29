83de47f8705667994554f98b25bad6c372e34b28
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-04-29 10:23:22 +0000

    🛠️ lib/webgl.js -> Updated uniform and attribute names 🛠️ sim/main.js -> Renamed 'frbuf' to 'framebuf', updated uniforms 🛠️ sim/shader.js -> Simplified shader code, updated variable names

--- sim/main.js
+++ sim/main.js
@@ -1,9 +1,9 @@
 // --- CREATE SHADOW FRAMEBUFFERS, TEXTURES AND LIGHT PROJECTION MATRICES ---
-const lights = { "res": 2048, "num": 4, "frbuf": [], "tex": [], "proj": [], "pos": [[-1, 2.9, -5.3], [0, 2.9, 0], [-3, 2.9, -3], [-5, 2.9, 0]], "look": [[-1, 0, -5], [0, 0, 0.3], [-3, 0, -3.3], [-5.3, 0, 0]] }
+const lights = { "res": 2048, "num": 4, "framebuf": [], "tex": [], "proj": [], "pos": [[-1, 2.9, -5.3], [0, 2.9, 0], [-3, 2.9, -3], [-5, 2.9, 0]], "look": [[-1, 0, -5], [0, 0, 0.3], [-3, 0, -3.3], [-5.3, 0, 0]] }
 
 for (let i = 0; i < lights["num"]; i++) {
     let lightData = createLight(gl, lights["res"], 160.0);
-    lights["frbuf"][i] = lightData[0];
+    lights["framebuf"][i] = lightData[0];
     lights["tex"][i] = lightData[1];
     lights["proj"][i] = lightData[2];
 }
@@ -21,11 +21,11 @@ const uniformLocationsShadow = getAllUniformLocations(gl, shadowProgram);
 // --- RENDER DEPTH MAPS ---
 function renderDepthMap() {
     for (let i = 0; i < lights["num"]; i++) {
-        prepareGLState(gl, lights["res"], lights["res"], shadowProgram, lights["frbuf"][i], gl.BACK);
+        prepareGLState(gl, lights["res"], lights["res"], shadowProgram, lights["framebuf"][i], gl.BACK);
 
-        gl.uniformMatrix4fv(uniformLocationsShadow["lightViewMatrix"], false, lookAtMat4f(lights["pos"][i], lights["look"][i], [0, 1, 0]));
-        gl.uniformMatrix4fv(uniformLocationsShadow["lightProjectionMatrix"], false, lights["proj"][i]);
-        gl.uniform3fv(uniformLocationsShadow["lightPosition"], lights["pos"][i]);
+        gl.uniformMatrix4fv(uniformLocationsShadow["l_viewmat"], false, lookAtMat4f(lights["pos"][i], lights["look"][i], [0, 1, 0]));
+        gl.uniformMatrix4fv(uniformLocationsShadow["l_projmat"], false, lights["proj"][i]);
+        gl.uniform3fv(uniformLocationsShadow["l_pos"], lights["pos"][i]);
 
         // Draw scene and drone for depth map
         drawDrawable(gl, sceneDrawable, attribLocationsShadow, uniformLocationsShadow, false);
@@ -38,17 +38,17 @@ function renderScene() {
     prepareGLState(gl, canvas.width, canvas.height, program, null, gl.BACK);
 
     // Set up view and projection matrices
-    gl.uniformMatrix4fv(uniformLocations["projectionmatrix"], false, projectionmatrix);
-    gl.uniformMatrix4fv(uniformLocations["viewmatrix"], false, attachedToDrone ? inv4Mat4f(multMat4f(yRotMat4f(degToRad(180)), droneDrawable["modelmatrix"])) : inv4Mat4f(cameraModelMatrix));
+    gl.uniformMatrix4fv(uniformLocations["projmat"], false, projectionmatrix);
+    gl.uniformMatrix4fv(uniformLocations["viewmat"], false, attachedToDrone ? inv4Mat4f(multMat4f(yRotMat4f(degToRad(180)), droneDrawable["modelmatrix"])) : inv4Mat4f(cameraModelMatrix));
 
     // Set up light view and projection matrices and shadow textures
     for (let i = 0; i < lights["num"]; i++) {
-        gl.uniformMatrix4fv(uniformLocations["lightViewMatrices"][i], false, lookAtMat4f(lights["pos"][i], lights["look"][i], [0, 1, 0]));
-        gl.uniformMatrix4fv(uniformLocations["lightProjectionMatrices"][i], false, lights["proj"][i]);
+        gl.uniformMatrix4fv(uniformLocations["l_viewmat"][i], false, lookAtMat4f(lights["pos"][i], lights["look"][i], [0, 1, 0]));
+        gl.uniformMatrix4fv(uniformLocations["l_projmat"][i], false, lights["proj"][i]);
 
         gl.activeTexture(gl.TEXTURE0 + i);
         gl.bindTexture(gl.TEXTURE_2D, lights["tex"][i]);
-        gl.uniform1i(uniformLocations["shadowTextures"][i], i);
+        gl.uniform1i(uniformLocations["l_tex"][i], i);
     }
 
     drawDrawable(gl, sceneDrawable, attribLocations, uniformLocations, true);
--- sim/shader.js
+++ sim/shader.js
@@ -1,91 +1,81 @@
-function getVertexShaderSource(numLights) {
+function getVertexShaderSource(num) {
     return `
         precision highp float;
 
-        attribute vec4 vertexposition;
-        attribute vec3 vertexnormal;
-        attribute vec2 texturecoordinate;
-        uniform mat4 modelmatrix;
-        uniform mat4 viewmatrix;
-        uniform mat4 projectionmatrix;
-        uniform mat4 lightViewMatrices[` + numLights + `];
-        uniform mat4 lightProjectionMatrices[` + numLights + `];
+        attribute vec4 vertexpos;
+        attribute vec3 vertexnorm;
+        attribute vec2 texcoord;
 
-        varying vec2 o_texturecoordinate;
-        varying vec3 o_vertexnormal;
-        varying vec4 o_shadowCoords[` + numLights + `];
+        uniform mat4 modelmat;
+        uniform mat4 viewmat;
+        uniform mat4 projmat;
+        uniform mat4 l_viewmat[` + num + `];
+        uniform mat4 l_projmat[` + num + `];
+
+        varying vec2 o_texcoord;
+        varying vec3 o_vertexnorm;
+        varying vec4 ol_coord[` + num + `];
         
         void main() {
-            o_texturecoordinate = texturecoordinate;
-            o_vertexnormal = normalize((modelmatrix * vec4(vertexnormal, 0.0)).xyz);
-            vec4 worldPosition = modelmatrix * vertexposition;
-            for (int i = 0; i < ` + numLights + `; i++) {
-                o_shadowCoords[i] = lightProjectionMatrices[i] * lightViewMatrices[i] * worldPosition;
-            }
-            gl_Position = projectionmatrix * viewmatrix * worldPosition;
+            gl_Position = projmat * viewmat * modelmat * vertexpos;
+
+            o_texcoord = texcoord;
+            o_vertexnorm = normalize((modelmat * vec4(vertexnorm, 0.0)).xyz);
+            for (int i = 0; i < ` + num + `; i++) 
+                ol_coord[i] = l_projmat[i] * l_viewmat[i] * modelmat * vertexpos;
         }
     `;
 }
 
-function getFragmentShaderSource(numLights, shadowMapResolution) {
+function getFragmentShaderSource(num, res) {
     return `
         precision highp float;
         
-        uniform sampler2D texture;
-        uniform sampler2D shadowTextures[` + numLights + `];
-        uniform vec3 lightPositions[` + numLights + `];
-        varying vec2 o_texturecoordinate;
-        varying vec3 o_vertexnormal;
-        varying vec4 o_shadowCoords[` + numLights + `];
+        uniform sampler2D tex;
+        uniform sampler2D l_tex[` + num + `];
+        uniform vec3 l_pos[` + num + `];
+
+        varying vec2 o_texcoord;
+        varying vec3 o_vertexnorm;
+        varying vec4 ol_coord[` + num + `];
         
-        float calculateShadow(vec4 shadowCoord, sampler2D shadowTexture, vec3 lightPosition) {
-            vec3 projCoords = shadowCoord.xyz / shadowCoord.w;
+        float calculateShadow(vec4 l_coord, sampler2D l_tex, vec3 l_pos) {
+            vec3 projcoord = l_coord.xyz / l_coord.w;
         
-            if(projCoords.z < -1.0 || projCoords.z > 1.0){
+            if(projcoord.z < -1.0 || projcoord.z > 1.0)
                 return 0.0;
-            }
             
             // Calculate the distance from the center of the frustum
-            float distanceFromCenter = length(projCoords.xy);
-            
+            float distanceFromCenter = length(projcoord.xy);
+
             // Calculate the shadow reduction factor based on the distance
             float shadowReduction = 1.0 - smoothstep(0.0, 1.0, distanceFromCenter);
-            
-            projCoords = projCoords * 0.5 + 0.5;
-            float closestDepth = texture2D(shadowTexture, projCoords.xy).r;
-            float currentDepth = projCoords.z;
-        
-            float bias = max(0.9 * (1.0 - dot(o_vertexnormal, normalize(lightPosition))), 0.000001);
+
+            projcoord = projcoord * 0.5 + 0.5;
+            float bias = max(0.9 * (1.0 - dot(o_vertexnorm, normalize(l_pos))), 0.000001);
         
             float shadow = 0.0;
-            vec2 texelSize = 1.0 / vec2(` + shadowMapResolution + `.0, ` + shadowMapResolution + `.0);
+            vec2 texelSize = 1.0 / vec2(` + res + `.0, ` + res + `.0);
             float totalWeight = 0.0;
-            const int neighborhoodsize = 4;
-            for (int x = -neighborhoodsize; x <= neighborhoodsize; x++) {
-                for (int y = -neighborhoodsize; y <= neighborhoodsize; y++) {
-                    float pcfDepth = texture2D(shadowTexture, projCoords.xy + vec2(x, y) * texelSize).r;
+            for (int x = -4; x <= 4; x++) {
+                for (int y = -4; y <= 4; y++) {
+                    float pcfDepth = texture2D(l_tex, projcoord.xy + vec2(x, y) * texelSize).r;
                     float weight = max(1.0 - length(vec2(x, y) * texelSize), 0.0);
-                    shadow += (currentDepth - bias > pcfDepth ? 0.0 : 1.0) * weight;
+                    shadow += (projcoord.z - bias > pcfDepth ? 0.0 : 1.0) * weight;
                     totalWeight += weight;
                 }
             }
             shadow /= totalWeight;
-            
-            // Apply the shadow reduction factor to the final shadow value
             shadow *= shadowReduction;
-            
             return shadow;
         }
         
         void main() {
-            vec4 textureColor = texture2D(texture, o_texturecoordinate);
-            
             float shadow = 0.0;
-            for (int i = 0; i < ` + numLights + `; i++) {
-                shadow = max(shadow, calculateShadow(o_shadowCoords[i], shadowTextures[i], lightPositions[i]));
-            }
+            for (int i = 0; i < ` + num + `; i++) 
+                shadow = max(shadow, calculateShadow(ol_coord[i], l_tex[i], l_pos[i]));
             
-            gl_FragColor = vec4(textureColor.rgb * shadow, 1.0);
+            gl_FragColor = vec4(texture2D(tex, o_texcoord).rgb * shadow, 1.0);
         }
     `;
 }
@@ -94,13 +84,14 @@ function getShadowVertexShaderSource() {
     return `
         precision highp float;
 
-        attribute vec4 vertexposition;
-        uniform mat4 modelmatrix;
-        uniform mat4 lightViewMatrix;
-        uniform mat4 lightProjectionMatrix;
+        attribute vec4 vertexpos;
+
+        uniform mat4 modelmat;
+        uniform mat4 l_viewmat;
+        uniform mat4 l_projmat;
 
         void main() {
-            gl_Position = lightProjectionMatrix * lightViewMatrix * modelmatrix * vertexposition;
+            gl_Position = l_projmat * l_viewmat * modelmat * vertexpos;
         }
     `;
 }
