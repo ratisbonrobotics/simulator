b82b39042388555b29c587cde636490f03c96673
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-05-05 15:36:54 +0000

    🛠️ sim/main.js -> Updated camera and drone settings

--- sim/main.js
+++ sim/main.js
@@ -22,10 +22,10 @@ const scene_configurations = [
             "framebuf": gl.createFramebuffer(),
             "tex": [],
             "proj": [],
-            "pos": [[-9, 2.9, -8.3], [-3, 2.9, -3.5], [-8, 2.9, -2]],
-            "look": [[-9, 0, -8], [-3, 0, -3.2], [-8.3, 0, 2.3]],
-            "dronemodelmatrix": modelMat4f(-2.0, 1.0, -2.0, 0.0, 0.0, 0.0, 0.01, 0.01, 0.01)
-        }
+            "pos": [[-5, 2.8, -9.3], [-4, 2.8, -2.5], [-1, 2.8, -6.8]],
+            "look": [[-5, 0, -9], [-3.1, 0, -3.2], [-1.3, 0, -6.5]]
+        },
+        "dronemodelmatrix": modelMat4f(-3.0, 1.0, -3.0, 0.0, 0.0, 0.0, 0.01, 0.01, 0.01)
     }
 ];
 const activeScene = scene_configurations[sceneIndex];
