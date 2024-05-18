77d1caabded182980230d7843fc783debb9f26d3
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-05-18 10:07:17 +0000

    🛠️ lib/webgl.js -> Removed drawDrawable function 🛠️ sim/main.js -> Integrated drawDrawable logic

--- sim/main.js
+++ sim/main.js
@@ -26,8 +26,34 @@ function renderScene() {
         inv4Mat4f(viewmatrix)
     );
 
-    drawDrawable(gl, scene_drawable, attrib_locs, uniform_locs, true);
-    drawDrawable(gl, drone_drawable, attrib_locs, uniform_locs, true);
+    gl.activeTexture(gl.TEXTURE16);
+    gl.uniform1i(uniform_locs["tex"], 16);
+
+    // draw scene
+    gl.uniformMatrix4fv(uniform_locs["modelmat"], false, scene_drawable["modelmatrix"]);
+    for (let p = 0; p < scene_drawable["vertexbuffer"].length; p++) {
+        connectBufferToAttribute(gl, gl.ARRAY_BUFFER, scene_drawable["vertexbuffer"][p]["verticies"], attrib_locs["vertexpos"], 3);
+        connectBufferToAttribute(gl, gl.ARRAY_BUFFER, scene_drawable["normalbuffer"][p], attrib_locs["vertexnorm"], 3);
+        connectBufferToAttribute(gl, gl.ARRAY_BUFFER, scene_drawable["texcoordbuffer"][p], attrib_locs["texcoord"], 2);
+
+        gl.bindTexture(gl.TEXTURE_2D, scene_drawable["texture"][p]);
+        gl.uniform3fv(uniform_locs["Ka"], scene_drawable["material"][p]["Ka"]);
+        
+        gl.drawArrays(gl.TRIANGLES, 0, scene_drawable["vertexbuffer"][p]["n_verticies"]);
+    }
+
+    // draw drone
+    gl.uniformMatrix4fv(uniform_locs["modelmat"], false, drone_drawable["modelmatrix"]);
+    for (let p = 0; p < drone_drawable["vertexbuffer"].length; p++) {
+        connectBufferToAttribute(gl, gl.ARRAY_BUFFER, drone_drawable["vertexbuffer"][p]["verticies"], attrib_locs["vertexpos"], 3);
+        connectBufferToAttribute(gl, gl.ARRAY_BUFFER, drone_drawable["normalbuffer"][p], attrib_locs["vertexnorm"], 3);
+        connectBufferToAttribute(gl, gl.ARRAY_BUFFER, drone_drawable["texcoordbuffer"][p], attrib_locs["texcoord"], 2);
+
+        gl.bindTexture(gl.TEXTURE_2D, drone_drawable["texture"][p]);
+        gl.uniform3fv(uniform_locs["Ka"], drone_drawable["material"][p]["Ka"]);
+        
+        gl.drawArrays(gl.TRIANGLES, 0, drone_drawable["vertexbuffer"][p]["n_verticies"]);
+    }  
 }
 
 // --- GET DATA FROM 3D FILES ---
