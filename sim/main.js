a8ac2a6003542bec33c002c262e3da24111cba43
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-05-18 09:33:57 +0000

    🛠️ sim/main.js -> Reformatted scene configurations 🛠️ sim/main.js -> Split long line in renderScene function

--- sim/main.js
+++ sim/main.js
@@ -1,14 +1,8 @@
 const urlParams = new URLSearchParams(window.location.search);
 const sceneIndex = urlParams.get("scene") ? parseInt(urlParams.get("scene")) : 0;
 const scene_configurations = [
-    {
-        "path": '/sim/data/scene0/scene.obj.gz',
-        "dronemodelmatrix": modelMat4f(-2.0, 1.0, -2.0, 0.0, 0.0, 0.0, 0.01, 0.01, 0.01)
-    },
-    {
-        "path": '/sim/data/scene1/scene.obj.gz',
-        "dronemodelmatrix": modelMat4f(-3.0, 1.0, -3.0, 0.0, 0.0, 0.0, 0.01, 0.01, 0.01)
-    }
+    {"path": '/sim/data/scene0/scene.obj.gz', "dronemodelmatrix": modelMat4f(-2.0, 1.0, -2.0, 0.0, 0.0, 0.0, 0.01, 0.01, 0.01)},
+    {"path": '/sim/data/scene1/scene.obj.gz', "dronemodelmatrix": modelMat4f(-3.0, 1.0, -3.0, 0.0, 0.0, 0.0, 0.01, 0.01, 0.01)}
 ];
 const activeScene = scene_configurations[sceneIndex];
 
@@ -25,14 +19,18 @@ function renderScene() {
 
     // Set up view and projection matrices
     gl.uniformMatrix4fv(uniform_locs["projmat"], false, projectionmatrix);
-    gl.uniformMatrix4fv(uniform_locs["viewmat"], false, attachedToDrone ? inv4Mat4f(multMat4f(yRotMat4f(degToRad(180)), drone_drawable["modelmatrix"])) : inv4Mat4f(viewmatrix));
+    gl.uniformMatrix4fv(uniform_locs["viewmat"], false, 
+        attachedToDrone ? 
+        inv4Mat4f(multMat4f(yRotMat4f(degToRad(180)), drone_drawable["modelmatrix"])) :
+        inv4Mat4f(viewmatrix)
+    );
 
     drawDrawable(gl, scene_drawable, attrib_locs, uniform_locs, true);
     drawDrawable(gl, drone_drawable, attrib_locs, uniform_locs, true);
 }
 
 // --- GET DATA FROM 3D FILES ---
-let scene_drawable = { "vertexbuffer": [], "normalbuffer": [], "texcoordbuffer": [], "texture": [], "material": [], "modelmatrix": modelMat4f(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0), "verticies": [], "keys": [] };
+let scene_drawable = {"vertexbuffer": [], "normalbuffer": [], "texcoordbuffer": [], "texture": [], "material": [], "modelmatrix": modelMat4f(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0), "verticies": [], "keys": [] };
 let drone_drawable = { "vertexbuffer": [], "normalbuffer": [], "texcoordbuffer": [], "texture": [], "material": [], "modelmatrix": activeScene["dronemodelmatrix"], "verticies": [], "keys": [] };
 
 (async function loadData() {
